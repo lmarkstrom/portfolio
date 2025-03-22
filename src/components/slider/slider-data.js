8399a113bc53cb68157079254fb492398aee35dd
Linus Markström <linusmarkstrom00@gmail.com>
2025-03-22 20:51:12 +0100

    fix deploy path to public

--- src/components/slider/slider-data.js
+++ src/components/slider/slider-data.js
@@ -1,24 +1,24 @@
 export const sliderData = [
     {
-        image: "/src/assets/slide_img/kex.png",
+        image: "/portfolio/images/kex.png",
         title: "Image recognition and Adversarial attacks",
         description: "Bachelor thesis with focus on Cyber Security and Computer Science.",
         button: ""
     },
     {
-        image: "/src/assets/slide_img/moviegram.png",
+        image: "/portfolio/images/moviegram.png",
         title: "Social media web-app",
         description: "Web-app using React, JS nad Firebase",
         button: ""
     },
     {
-        image: "/src/assets/slide_img/ywh.svg",
+        image: "/portfolio/images/ywh.svg",
         title: "Geodata webb-app",
         description: "Web-app using several Google API´s and SQLite",
         button: ""
     },
     {
-        image: "/src/assets/slide_img/game1.png",
+        image: "/portfolio/images/game1.png",
         title: "2D Java game-engine",
         description: "2D-gamae engine from scratch in Java",
         button: ""
